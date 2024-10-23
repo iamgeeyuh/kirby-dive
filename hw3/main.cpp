91a2b676a6434ae1fc8d8d250432e57e9015c766
Jia Huang <jiahuang2468@gmail.com>
2024-10-23 13:37:59 -0400

    Initial Commit

---
+++ hw3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hw3
+//
+//  Created by Jia Huang on 10/23/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
